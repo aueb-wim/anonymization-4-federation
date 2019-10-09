4559706b573f96856abe5139094b3289e251fe6f
iosif <iosifspart@yahoo.gr>
2019-10-09 11:00:02 +0300

    added sha3 hashing in python script

--- README.md
+++ README.md
@@ -3,18 +3,22 @@ Anonymizing data by hashing id's while using randomly generated numbers
 
 The current module deletes the id's of a relational i2b2-schema database while preserving the entities' resolution and relationships among them. Having as input an i2b2 database, the output is a replicated database with randomly generated patient and encounter id's which are not possible to be linked to the input db's id's. This applies in both ways, meaning the output id's cannot be re-generated in the same way therefore cannot be linked to those of an already anonymized database. To succeed that, we add to each id a random_number from 1 to 100 and then we hash it.
 
-new_id = MD5(id + random_number)
+new_id = MD5(id + random_number) or 
+new_id = SHA3_224(id + random_number)
 
 For the case where the data are already in CSV we randomly hash the id column.
 
-Currently the hashing function that is being used is MD5. In the future we may use others like SHA256, SHA512. Postgres provides several crypto functions (https://www.postgresql.org/docs/9.4/pgcrypto.html), therefore MD5 can be easily replaced by a more secure one. On the other hand, even if someone manages to decrypt (dehash) she will get the sum of the id and a random_number and not the id itself.
+Currently the hashing function that is being used is MD5 and SHA3-224. In the future we may use others like SHA256, SHA512. Postgres provides several crypto functions (https://www.postgresql.org/docs/9.4/pgcrypto.html), therefore MD5 can be easily replaced by a more secure one. On the other hand, even if someone manages to decrypt (dehash) she will get the sum of the id and a random_number and not the id itself.
 
 ## Python script usage
 Give the command in terminal
 ```shell
-python anonymize_csv.py <inputcsvpath> <outputcsvpath>
+./anonymize_csv.py <inputcsvpath> <outputcsvpath>
 ```
 **Options**
 
-- `-c` or `--columns` - 
+- `-c` or `--columns` -
 By default the script will hash the first column of the input csv. If we want to hash certain columns, i.e. 2nd and 3rd column, we add `-c 2 3` or `--columns 2 3`.
+
+- `-m` or `--method` -
+The hash function that will be used. Select between `md5` and `sha3`. As default is the `sha3` method.
--- anonymize_csv.py
+++ anonymize_csv.py
@@ -1,64 +1,72 @@
+#!/usr/bin/env python3
+
 #For the case where we have the harmonized data already in CSV we randomly hash its id column.
 import sys
 import csv
 import hashlib
+import sha3
 import argparse
 from copy import copy
 from random import *
 
-def hash_id(str_id):
-	#  case where str_id is not a number
-	try:
-		random_num = float(str_id) + random()*100
-		random_num_str = str(random_num).encode()
-	except:
-		random_num_str = str_id + str(random()*100)
-		random_num_str = random_num_str.encode()
-	
-	random_hash_str = hashlib.md5(random_num_str)
-	print("Row num:", str_id, "Random num: ", random_num_str, "Random Hash: ", random_hash_str.hexdigest())
-	return random_hash_str.hexdigest()
+
+def hash_id(str_id, method='sha3'):
+    #  case where str_id is not a number
+    try:
+        random_num = float(str_id) + random()*100
+        random_num_str = str(random_num).encode()
+    except:
+        random_num_str = str_id + str(random()*100)
+        random_num_str = random_num_str.encode()
+    if method == 'md5':
+        random_hash_str = hashlib.md5(random_num_str)
+    elif method == 'sha3':
+        random_hash_str = sha3.sha3_224(random_num_str)
+    # print("Row num:", str_id, "Random num: ", random_num_str, "Random Hash: ", random_hash_str.hexdigest())
+    return random_hash_str.hexdigest()
 
 
-def anonymize_csv(inputpath, outputpath, columns):
-	print("We will anonymize file ", inputpath, " assuming its first column is its id.")
-	lookup_id = {}
-	csv_new = []
-	with open(inputpath, mode='r') as csv_file:
-		csv_reader = csv.reader(csv_file, delimiter=',')
-		line=0
-		for row in csv_reader:
-			new_row = copy(row)
-			if line==0: #first row has the headers...
-				csv_new.append(row)
-				print("First row: Headers...")		
-			else:
-				for column in columns:
-				    lookup_id.setdefault(row[column], hash_id(row[column]))
-				    new_row[column] = lookup_id[row[column]]
-				csv_new.append(new_row)
-			line += 1
-	with open(outputpath, mode= 'w') as csv_file:
-		csv_writer = csv.writer(csv_file, delimiter=',')
-		for row in csv_new:
-			csv_writer.writerow(row)
+def anonymize_csv(inputpath, outputpath, columns, method):
+    print("We will anonymize file ", inputpath, " assuming its first column is its id.")
+    lookup_id = {}
+    csv_new = []
+    with open(inputpath, mode='r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line = 0
+        for row in csv_reader:
+            new_row = copy(row)
+            if line == 0:  # first row has the headers...
+                csv_new.append(row)
+                print("First row: Headers...")
+            else:
+                for column in columns:
+                    lookup_id.setdefault(row[column], hash_id(row[column], method))
+                    new_row[column] = lookup_id[row[column]]
+                csv_new.append(new_row)
+            line += 1
+    with open(outputpath, mode='w') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',')
+        for row in csv_new:
+            csv_writer.writerow(row)
 
 
 def main():
-	"CLI for anonymazation script"
-	# Create a parser
-	parser = argparse.ArgumentParser(description='Script for anonymization \
-                                     of hospital EHR csv.') 
-	parser.add_argument('inputpath', type=str,
-					help='initial ehr csv to be anonymized')
-	parser.add_argument('outputpath', type=str,
-	                    help='pathanonymized csv filepath')
-	parser.add_argument('-c', '--columns', type=int, nargs='*', default=[1])
-	args = parser.parse_args(sys.argv[1:])
-	# substracting 1 to have python correct list indexes
-	columns = [x - 1 for x in args.columns]
-	# call the anonymiza function 
-	anonymize_csv(args.inputpath, args.outputpath, columns)
+    "CLI for anonymazation script"
+    # Create a parser
+    parser = argparse.ArgumentParser(description='Script for anonymization \
+                                     of hospital EHR csv.')
+    parser.add_argument('inputpath', type=str,
+                        help='initial ehr csv to be anonymized')
+    parser.add_argument('outputpath', type=str,
+                        help='pathanonymized csv filepath')
+    parser.add_argument('-m', '--method', choices=['md5', 'sha3'],
+                        default='sha3', help='hash fucntion to be used')
+    parser.add_argument('-c', '--columns', type=int, nargs='*', default=[1])
+    args = parser.parse_args(sys.argv[1:])
+    # substracting 1 to have python correct list indexes
+    columns = [x - 1 for x in args.columns]
+    # call the anonymiza function
+    anonymize_csv(args.inputpath, args.outputpath, columns, args.method)
 
 if __name__ == '__main__':
-	main()
+    main()
